b2b1b26403a30bc284782cb2be9fd0248b795349
alex <none>
2020-06-01 22:15:12 +0300

    Добавить прототип скачивания со всех папк ящика.
    На данный момент - это большой костыль. Сейчас нормально работает только с mail.ru (так как для этих ящиков и создается программа).

--- email_loader_core.py
+++ email_loader_core.py
@@ -35,7 +35,22 @@ class MsgLoader:
         self._downloaded_msg = []
         self.login = ""
 
-    def connect_mailbox(self, login: str, password: str, mailbox="inbox"):
+    def get_mail_dir(self, login: str, password: str,):
+        self.login = login
+        mailserver = login.split('@')[1]
+        if login.split("@")[1] in ["bk.ru", "inbox.ru"]:
+            mailserver = "mail.ru"
+        try:
+            self.mail = imaplib.IMAP4_SSL('imap.' + mailserver)
+            self.mail.login(login, password)
+            logger.info("Подключение к ящику {mailbox} успешно".format(mailbox=login))
+            return self.mail.list()[1]
+        except:
+            logger.error("Подключение к ящику {mailbox} не удалось. Он будет пропущен".format(mailbox=login))
+            logger.debug("Ошибка: ", exc_info=True)
+            return False
+
+    def connect_mailbox(self, login: str, password: str, mailbox="INBOX"):
         self.login = login
         mailserver = login.split('@')[1]
         if login.split("@")[1] in ["bk.ru", "inbox.ru"]:
@@ -195,8 +210,11 @@ class MsgLoader:
             return False
 
     def create_imap_date(self, p_datestr):
-        str_date = re.findall(r'\d+\s\w+\s\d\d\d\d\s\d\d:\d\d:\d\d', p_datestr)
-        return datetime.datetime.strptime(str_date[0], "%d %b %Y %H:%M:%S")
+        try:
+            str_date = re.findall(r'\d+\s\w+\s\d\d\d\d\s\d\d:\d\d:\d\d', p_datestr)
+            return datetime.datetime.strptime(str_date[0], "%d %b %Y %H:%M:%S")
+        except:
+            return datetime.datetime.now()
 
     def download_msg_by_period(self, p_startdate, p_enddate=datetime.datetime.today(),
                                is_download_msg=True, is_download_payload=True):
@@ -220,6 +238,12 @@ class MsgLoader:
                         self.msgParser.save_msg_payload((uid_msg, self.login, msg))
                 else:
                     break
+
+    def download_msg_by_period_test(self, p_startdate, p_enddate=datetime.datetime.today(),
+                               is_download_msg=True, is_download_payload=True):
+        for dir in self.mail.list()[1]:
+            print(dir.split()[2].decode('utf-8'))
+            self.set_mailbox(dir)
 
 class MsgParser:
     """
@@ -258,8 +282,11 @@ class MsgParser:
         """
         TODO: обработка ошибок на неудачу при разборе кодировки заголовка
         """
-        email_msg = self.read_msg(p_msg)
-        return str(email.header.make_header(email.header.decode_header(email_msg['Date'])))
+        try:
+            email_msg = self.read_msg(p_msg)
+            return str(email.header.make_header(email.header.decode_header(email_msg['Date'])))
+        except:
+            return ""
 
     def save_msg(self, p_msg):
         try:
@@ -494,9 +521,14 @@ if __name__ == "__main__":
             enddate = datetime.datetime.strptime(enddate, DEFAULT_DATE_FORMAT)
             logger.info("Начальная дата {}, конечная дата {}!".format(startdate, enddate))
             for mail in dict_mail.keys():
-                if not EDL.connect_mailbox(mail, dict_mail[mail]):
-                    continue
-                EDL.download_msg_by_period(startdate, enddate)
+                dir_list = EDL.get_mail_dir(mail, dict_mail[mail])
+                print(dir_list)
+                for mail_dir in dir_list:
+                    EDL.mail.logout()
+                    print(mail_dir.split()[2].decode('utf-8'))
+                    if not EDL.connect_mailbox(mail, dict_mail[mail], mailbox=mail_dir.split()[2].decode('utf-8')):
+                        continue
+                    EDL.download_msg_by_period(startdate, enddate)
 
     elif args_list.is_all_msg_download:
         logger.info("Работа в режиме скачивания всех писем с ящика запущена!")
